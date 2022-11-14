6aede9f3c65146002b747fcbf10588d4d098d433
Rack Saind <@abraham.saindou@laplateforme.io>
2022-11-14 18:56:02 +0100

    added everythine from start

--- front/src/components/Card.js
+++ front/src/components/Card.js
@@ -4,8 +4,29 @@ import "./Card.css"
 
 const Card = () => {
     return (
-        <div>
-            {/*    Abraham tu codes ici   */}
+        < div className="card">
+            {<article className="cardactiv1">
+                <h3>110m haies</h3>
+                <p> Nom de l'athlète1</p>
+                <p> Nom de l'athlète2</p>
+                <p> Vainqueur et temps</p>
+                <p> Heure de l'épreuve</p>
+            <img src="./flag-icons-main/flags/1x1/ac.svg" alt=""/>
+            <img src="./flag-icons-main/flags/1x1/ac.svg" alt=""/>
+            <a class="link1" href="https://www.france.tv/france-2/direct.html">Regarder l'évènement sur France 2</a>
+
+                </article>}
+            {<article className="cardactiv2">
+                <h3>110m haies</h3>
+                <p> Nom de l'athlète1</p>
+                <p> Nom de l'athlète2</p>
+                <p> Vainqueur et temps</p>
+                <p> Heure de l'épreuve</p>
+            <img src="./flag-icons-main/flags/1x1/ac.svg" alt=""/>
+            <img src="./flag-icons-main/flags/1x1/ac.svg" alt=""/>
+            <a class="link1" href="https://www.france.tv/france-2/direct.html">Regarder l'évènement sur France 2</a>
+
+                </article>}
         </div>
     );
 };
